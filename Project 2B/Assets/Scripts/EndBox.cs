74a764fc3786ec2c6c353f6f194e0e939f2559eb
sggu <shgudwls99@gmail.com>
2019-03-17 19:49:44 -0400

    Created a new branch for future changes

---
+++ Project 2B/Assets/Scripts/EndBox.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This script controls the interaction between the 
+/// apple and the endbox and how it affects the life
+/// </summary>
+public class EndBox : MonoBehaviour {
+
+    private int lives = 3;
+    public Text livesText;
+    [Space]
+    public AudioSource sfx;
+
+    void Start()
+    {
+        setText();        
+    }
+
+    void Update()
+    {
+        if (lives == 0)
+        {
+            SceneManager.LoadScene("SampleScene");
+        } 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if the apple hits the endbox the life goes down by one
+        if (collision.gameObject.tag == "Apple")
+        {            
+            lives--;
+            setText();
+            sfx.Play();
+        }
+    }
+
+    private void setText()
+    {
+        livesText.text = "Lives: " + lives;
+    }
+}
---
+++ Project 2B/Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script controls the random movement of the apple tree
+/// </summary>
+public class RandomMovement : MonoBehaviour {
+    [Header("Settings")]
+    public float speed;
+    public float leftAndRightEdge;
+    [Space]
+    public GameObject applePrefab;
+
+    private void Start()
+    {
+        //will spawn apples every two seconds
+        InvokeRepeating("SpawnObject", 2, 2);
+    }
+
+    public void SpawnObject()
+    {
+        Instantiate(applePrefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //if the apple tree hits the boundaries it will be forced to turn
+        if (pos.x < -leftAndRightEdge || pos.x > leftAndRightEdge) speed *= -1;
+    }
+    private void FixedUpdate()
+    {
+        //random chance of turning the other direction
+        float directionChangeChance = 0.1f;
+        if (Random.value <= directionChangeChance) speed *= -1;
+    }
+}
